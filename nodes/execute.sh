4662c418cd1cc0b18205ebb6b1bdb0f0f65b4713
keenan-burnett <keenburn2004@gmail.com>
2017-08-16 15:55:35 -0400

    Addeda script for running the appropriate launch file, roscore, rosrun image transport, and starting the rosbag.

---
+++ nodes/execute.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Run this under Zeus/nodes/
+# Make sure this script is executable with chmod +x execute.sh
+# Provide this folder with a symbolic link to your George1.bag rosbag,
+# name it George1.bag ex: ln ~/catkin_ws/src/lane_detector/George1.bag George1.bag
+node="$1"
+roscore &
+sleep 1
+if [ "$node" == "lane" ]; then 
+	roslaunch lane_detector/lane_detection.launch &
+elif [ "$node" == "kitti" ]; then 
+	roslaunch lane_detector/kitti_lane_detection.launch &
+elif [ "$node" == "stop" ]; then 
+	roslaunch lane_detector/stop_line_detection.launch &
+else
+	echo "Usage: ./execute.sh <lane|stop|kitti>"
+	exit
+fi
+
+sleep 1
+rosrun image_transport republish compressed in:=/RawStereoToRos/left/image_raw raw out:=camera/image_raw &
+sleep 1
+rosbag play -s 1070 -u 50 George1.bag
+kill $(ps aux | grep 'roscore' | awk '{print $2}')
+kill $(ps aux | grep 'roslaunch' | awk '{print $2}')
+kill $(ps aux | grep 'republish' | awk '{print $2}')
+

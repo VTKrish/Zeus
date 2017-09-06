d7aa2e250840b1e6cac4141ced1afdaff1134a11
sep <sepsamavi@github.com>
2017-09-06 10:51:01 -0400

    added ipm

--- nodes/execute.sh
+++ nodes/execute.sh
@@ -12,15 +12,17 @@ elif [ "$node" == "kitti" ]; then
 	roslaunch lane_detector/kitti_lane_detection.launch &
 elif [ "$node" == "stop" ]; then 
 	roslaunch lane_detector/stop_line_detection.launch &
+elif [ "$node" == "ipm" ]; then 
+	roslaunch lane_detector/ipm_lane_detection.launch &
 else
-	echo "Usage: ./execute.sh <lane|stop|kitti>"
+	echo "Usage: ./execute.sh <lane|stop|kitti|ipm>"
 	exit
 fi
 
 sleep 1
 rosrun image_transport republish compressed in:=/RawStereoToRos/left/image_raw raw out:=camera/image_raw &
 sleep 1
-rosbag play -s 1070 -u 50 George1.bag
+rosbag play -s 450 -u 30 George1.bag
 kill $(ps aux | grep 'roscore' | awk '{print $2}')
 kill $(ps aux | grep 'roslaunch' | awk '{print $2}')
 kill $(ps aux | grep 'republish' | awk '{print $2}')
